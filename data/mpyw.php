a8546d4e7205219c0e32bee5b65035bc018199e4
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-04-03 19:32:25 +0900

    確率変更

--- data/mpyw.php
+++ data/mpyw.php
@@ -1,15 +1,20 @@
 <?php
 
 // コメントを付けておきたいのでphpの配列のまま
+// 整数
+// N   70%
+// R   20%
+// SR  9.5%
+// SSR 0.5%
 return [
-    'http://nyaruko.com' => 30,                                                       // ニャル子
-    'http://www.gochiusa.com' => 30,                                                  // ごちうさ
-    'https://twitter.com/mpyw' => 8,                                                 // 元祖ホモの輪
-    'https://github.com/mpyw' => 8,                                                  // GitHub
-    'https://qiita.com/mpyw' => 8,                                                   // Qiita
-    'http://mpyw.hateblo.jp' => 8,                                                   // はてブロ
-    'https://codeiq.jp/magazine/2015/11/31368/' => 5,                                // PHP界のプリンス
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 1,    // 実務経験
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 1,    // くだらん解答
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 1,    // ごちうさ難民的に
+    'http://nyaruko.com' => 350,                                                        // ニャル子
+    'http://www.gochiusa.com' => 350,                                                   // ごちうさ
+    'https://twitter.com/mpyw' => 50,                                                   // 元祖ホモの輪
+    'https://github.com/mpyw' => 50,                                                    // GitHub
+    'https://qiita.com/mpyw' => 50,                                                     // Qiita
+    'http://mpyw.hateblo.jp' => 50,                                                     // はてブロ
+    'https://codeiq.jp/magazine/2015/11/31368/' => 35,                                  // PHP界のプリンス
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 30,      // くだらん解答
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 30,      // ごちうさ難民的に
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 5,       // 実務経験
 ];
