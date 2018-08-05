595f8865c5f79b333b55487433be661a9946b05a
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-08-05 17:54:22 +0900

    PHP界のプリンスがリンク切れになってたのでArchiveに向けた

--- data/mpyw.php
+++ data/mpyw.php
@@ -7,14 +7,14 @@
 // SR  9.5%
 // SSR 0.5%
 return [
-    'http://nyaruko.com' => 350,                                                        // ニャル子
-    'http://www.gochiusa.com' => 350,                                                   // ごちうさ
-    'https://twitter.com/mpyw' => 50,                                                   // 元祖ホモの輪
-    'https://github.com/mpyw' => 50,                                                    // GitHub
-    'https://qiita.com/mpyw' => 50,                                                     // Qiita
-    'http://mpyw.hateblo.jp' => 50,                                                     // はてブロ
-    'https://codeiq.jp/magazine/2015/11/31368/' => 35,                                  // PHP界のプリンス
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 30,      // くだらん解答
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 30,      // ごちうさ難民的に
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 5,       // 実務経験
+    'http://nyaruko.com' => 350,                                                                    // ニャル子
+    'http://www.gochiusa.com' => 350,                                                               // ごちうさ
+    'https://twitter.com/mpyw' => 50,                                                               // 元祖ホモの輪
+    'https://github.com/mpyw' => 50,                                                                // GitHub
+    'https://qiita.com/mpyw' => 50,                                                                 // Qiita
+    'http://mpyw.hateblo.jp' => 50,                                                                 // はてブロ
+    'https://web.archive.org/web/20170408045701/https://codeiq.jp/magazine/2015/11/31368/' => 35,   // PHP界のプリンス (Internet Archive)
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 30,                  // くだらん解答
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 30,                  // ごちうさ難民的に
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 5,                   // 実務経験
 ];
