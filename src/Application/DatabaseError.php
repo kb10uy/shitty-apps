cee2c4cb04e684985d7b85b768d447c8a07111ca
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-11-11 22:29:02 +0900

    html

--- src/Application/DatabaseError.php
+++ src/Application/DatabaseError.php
@@ -8,16 +8,21 @@ use Library\Kb10uyApplication;
 
 class DatabaseError extends Kb10uyApplication
 {
+    private const GENERAL_RESPONSE = 'データベース接続確立エラー';
+    private const HTML_RESPONSE = '<!doctype html><html><head><title>データベース接続確立エラー</title></head><body><h1>データベース接続確立エラー</h1></body></html>';
     public function handle(): ResponseInterface
     {
-        $mime = 
-            substr($this->request->getServerParams()['REQUEST_URI'], 1)
-            ?: $this->request->getHeader('Accept')[0]
-            ?: 'text/html';
+        $default = 'text/html';
+        $accept = $this->request->getHeader('Accept')[0];
+        $query = substr($this->request->getServerParams()['REQUEST_URI'], 1);
+        if ($accept) {
+            $accept = trim(explode(',', $accept)[0]);
+        }
+        $mime = $query ?: $accept ?: $default;
         $response = $this->response
             ->withHeader('Content-Type', $mime)
             ->withStatus(200);
-        $response->getBody()->write("データベース接続確立エラー");
+        $response->getBody()->write($mime === 'text/html' ? self::HTML_RESPONSE : self::GENERAL_RESPONSE);
         return $response;
     }
 }
