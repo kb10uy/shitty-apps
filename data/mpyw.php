44ae484cc2abf46b460c8e1bf9927c00dddbdb59
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-12-27 14:00:56 +0900

    retab

--- data/mpyw.php
+++ data/mpyw.php
@@ -7,15 +7,15 @@
 // SR  9.5%
 // SSR 0.5%
 return [
-    'http://nyaruko.com' => 350,                                                                    														// ニャル子
-    'http://www.gochiusa.com' => 350,                                                               														// ごちうさ
-    'https://twitter.com/mpyw' => 50,                                                               														// 元祖ホモの輪
-    'https://github.com/mpyw' => 50,                                                                														// GitHub
-    'https://qiita.com/mpyw' => 50,                                                                 														// Qiita
-    'http://mpyw.hateblo.jp' => 50,                                                                 														// はてブロ
-    'https://web.archive.org/web/20170408045701/https://codeiq.jp/magazine/2015/11/31368/' => 35,   														// PHP界のプリンス (Internet Archive)
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 30,                  														// くだらん解答
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 30,                  														// ごちうさ難民的に
+    'http://nyaruko.com' => 350,                                                                                                // ニャル子
+    'http://www.gochiusa.com' => 350,                                                                                           // ごちうさ
+    'https://twitter.com/mpyw' => 50,                                                                                           // 元祖ホモの輪
+    'https://github.com/mpyw' => 50,                                                                                            // GitHub
+    'https://qiita.com/mpyw' => 50,                                                                                             // Qiita
+    'http://mpyw.hateblo.jp' => 50,                                                                                             // はてブロ
+    'https://web.archive.org/web/20170408045701/https://codeiq.jp/magazine/2015/11/31368/' => 35,                               // PHP界のプリンス (Internet Archive)
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11152944766' => 30,                                              // くだらん解答
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13173759308' => 30,                                              // ごちうさ難民的に
     'https://web.archive.org/web/20181111004435/https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13198470468' => 30,   // いってる意味がよくわかりません (Internet Archive)
-    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 5,                   														// 実務経験
+    'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13149033688' => 5,                                               // 実務経験
 ];
